f18e64d42fed5f43929bf265b7679e5a38cd34c9
ditorre <ditorre@CCTL130-08>
2022-10-24 19:56:06 +0200

    Vigenere

--- CifradoVigenere/Vigenere.cs
+++ CifradoVigenere/Vigenere.cs
@@ -15,15 +15,36 @@ namespace CifradoVigenere
         {
         }
 
-        public override Mensaje encriptar(Mensaje mensaje,Mensaje Clave)
+        public override Mensaje desEncriptar(Mensaje mensaje, Mensaje clave)
         {
             return null;
         }
 
-        public override Mensaje desEncriptar(Mensaje mensaje, Mensaje Clave)
+        //agoritmo de encriptacion
+        private char[] encriptacion(Mensaje mensaje, Mensaje clave)
         {
-            return null;
+            char[] texto = mensaje.textoC();
+            char[] cifrado = new char[texto.Length];
+            for (int i = 0; i < texto.Length; i++)
+            {
+                if (texto[i] != ' ')
+                {
+                    cifrado[i] = Convert.ToChar((Convert.ToInt16(texto[i]) + clave.textoC()[i%(clave.textoC().Length)] - 2*Convert.ToInt16('a')) % 26 + Convert.ToInt16('a'));
+                }
+                
+            }
+
+            return cifrado;
+        }
+        //Convierte la clave en el tipo requerido por la impelmentación (int)
+        private int codificarClave(Mensaje clave)
+        {
+            return Convert.ToInt32(clave.textoS());
         }
 
+        public override Mensaje encriptar(Mensaje mensaje, Mensaje clave)
+        {
+            return new Mensaje(encriptacion(mensaje,clave));
+        }
     }
 }
